6e278dc5a5f4b42f6cadde004c3b2fe9f013834d
BusterStatus <matthew.serre@gmail.com>
2019-06-27 13:06:55 -0700

    Add routes to reservation controller

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -17,52 +17,58 @@ class ReservationsController < ApplicationController
     end
 
     post '/reservations' do
-        @reservation = Reservation.create(params, user_id: session[:user_id])
+        if params[:name] == "" || params[:date] == "" || params[:resource] == "" || params[:contact] == ""
+            flash[:blank_fields] = "No fields can be left blank."
+            redirect to '/reservations/new'
+        else
+            @reservation = Reservation.create(params, user_id: session[:user_id])
+            redirect "/reservations/#{@reservation.id}"
+        end
     end
 
-    # get '/reservations/:id' do
-    #     @user = User.find_by(id: session[:user_id])
-    #     if !@user.nil?
-    #         @tweet = Tweet.find(params[:id])
-    #         erb :'/reservations/show'
-    #     else
-    #       redirect '/login'
-    #     end
-    # end
 
-    # get '/reservations/:id/edit' do
-    #     @user = User.find_by(id: session[:user_id])
-    #     if !@user.nil?
-    #         @tweet = Tweet.find(params[:id])
-    #         erb :'/reservations/edit'
-    #     else
-    #         redirect '/login'
-    #     end
-    # end
+    get '/reservations/:id' do
+        if logged_in?
+            @reservation = Reservation.find(params[:id])
+            erb :'/reservations/show'
+        else
+          redirect '/login'
+        end
+    end
 
-    # patch '/reservations/:id' do
-    #     @tweet = Tweet.find(params[:id])
-    #     if @tweet
-    #         if params[:content] != ""
+    get '/reservations/:id/edit' do
+        @user = User.find_by(id: session[:user_id])
+        if !@user.nil?
+            @tweet = Tweet.find(params[:id])
+            erb :'/reservations/edit'
+        else
+            redirect '/login'
+        end
+    end
 
-    #             @tweet.update(content: params[:content])
-    #             redirect "/reservations/#{@tweet.id}"
-    #         else
-    #             redirect "/reservations/#{@tweet.id}/edit"
-    #         end
-    #     end
-    # end
+    patch '/reservations/:id' do
+        @tweet = Tweet.find(params[:id])
+        if @tweet
+            if params[:content] != ""
 
-    # delete '/reservations/:id' do
-    #     @user = User.find_by(id: session[:user_id])
-    #     if !@user.nil?
-    #         @tweet = Tweet.find(params[:id])
-    #         if @tweet.user_id == session[:user_id]
-    #             @tweet.delete
-    #             redirect '/reservations'
-    #         end
-    #     else
-    #         redirect '/login'
-    #     end
-    # end
+                @tweet.update(content: params[:content])
+                redirect "/reservations/#{@tweet.id}"
+            else
+                redirect "/reservations/#{@tweet.id}/edit"
+            end
+        end
+    end
+
+    delete '/reservations/:id' do
+        @user = User.find_by(id: session[:user_id])
+        if !@user.nil?
+            @tweet = Tweet.find(params[:id])
+            if @tweet.user_id == session[:user_id]
+                @tweet.delete
+                redirect '/reservations'
+            end
+        else
+            redirect '/login'
+        end
+    end
 end
